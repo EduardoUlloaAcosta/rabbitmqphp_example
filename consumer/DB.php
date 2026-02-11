b4bd4259b0b489c4c231e13bd1b7bea890da4609
bp558DB <bp558@njit.edu>
2026-02-11 16:46:35 -0500

    starting consumer files for rabbitmq

---
+++ consumer/DB.php
@@ -0,0 +1,14 @@
+<?php
+//Brian Patoilo 2/11/26 database config caller
+
+require_once __DIR__ . '/config.php';
+
+function getDB() {
+    $db = new mysql(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if ($db->connect_error) {
+        throw new Exception("database not there idiot" . db->connect_error);
+    }
+    $db->set_charset("utf8mb4");
+    return $db;
+}
+?>
