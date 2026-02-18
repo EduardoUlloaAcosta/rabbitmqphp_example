5b94289938586d29bfc736850833d9dd611b5ff5
bp558DB <bp558@njit.edu>
2026-02-18 17:53:47 -0500

    checking config file

--- consumer/config.php
+++ consumer/config.php
@@ -1,16 +1,16 @@
 <?php
 //Brian Patoilo 2/11/26 Written to define things for project
-define('RABBITMQ_HOST', '100.71.114.73');   // <-- Change to your RabbitMQ VM's IP
+define('RABBITMQ_HOST', '100.71.114.73');
 define('RABBITMQ_PORT', 5672);
-define('RABBITMQ_USER', 'admin');      // <-- Change to your RabbitMQ username
-define('RABBITMQ_PASS', '123');      // <-- Change to your RabbitMQ password
+define('RABBITMQ_USER', 'admin');
+define('RABBITMQ_PASS', '123');
 define('RABBITMQ_VHOST', '/');
 
-// ─── Database Connection (local on this VM) ───
+// database connection
 define('DB_HOST', 'localhost');
 define('DB_USER', 'testUser');
 define('DB_PASS', '123');
 define('DB_NAME', 'meal_planner');
-// ─── Queue Names ───
+
 define('QUEUE_NAME', 'db_queue');
 ?>
